1ea243d43b98e375a87bd6abbd2e87dde3716c6a
chialatte2129 <mr.jiadewang@gmail.com>
2024-01-23 21:12:04 +0000

    Add Readme & analyze page

--- README.md
+++ README.md
@@ -1,46 +1,97 @@
-# URL Shortener 短網址服務
+# URL Shortener Service
 
-一個基於Flask，利用sqlite、Redis實現的短網址服務。並透過Docker解決不同環境下的佈署問題。
+A URL shortener service based on `Flask`, implemented using `bootstrap`, `sqlite` and `Redis`. Deployment across different environments is facilitated through `Docker`.
 
+<div align="center">
+	<img width="80" src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png" alt="Python" title="Python"/>
+	<img width="80" src="https://user-images.githubusercontent.com/25181517/183423775-2276e25d-d43d-4e58-890b-edbc88e915f7.png" alt="Flask" title="Flask"/>
+	<img width="80" src="https://user-images.githubusercontent.com/25181517/182884894-d3fa6ee0-f2b4-4960-9961-64740f533f2a.png" alt="redis" title="redis"/>
+	<img width="80" src="https://user-images.githubusercontent.com/25181517/183898054-b3d693d4-dafb-4808-a509-bab54cf5de34.png" alt="Bootstrap" title="Bootstrap"/>
+	<img width="80" src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/82df4543-236b-4e45-9604-5434e3faab17" alt="SQLite" title="SQLite"/>
+</div>
 
-## 環境需求
+## Features
 
-需在有Docker與docker-compose的環境執行
+1. Submit url to get shorter URL
+<p align="center" width="100%">
+    <img width="70%" src="/public/main_page.png"> 
+</p>
 
-## 佈署服務
+2. Show redirection url list
+<p align="center" width="100%">
+    <img width="70%" src="/public/short_url_list.png"> 
+</p>
 
-1. 將專案從Server載到本機環境
-```
+3. Analyzed of each url
+<p align="center" width="100%">
+    <img width="70%" src="/public/url_analyze.png"> 
+</p>
+
+## Caching
+
+For optimal operational efficiency, we minimize database reads during redirection by first checking for records in the redis cache. If not found, we proceed with a database query. For logging, we utilize a Redis Queue, processed by a separate thread, ensuring real-time responses.
+
+<p align="center" width="100%">
+    <img width="70%" src="/public/URL_Shorterner.drawio.png"> 
+</p>
+
+## Environment Requirements
+
+Execution requires an environment with Docker and docker-compose.
+
+## Service Deployment
+
+1. Clone the project from the server to your local environment:
+
+```sh
 git clone https://github.com/chialatte2129/url-shortener.git
 ```
-2. 進入專案資料夾
-```
+
+2. Enter the project folder
+
+```sh
 cd url-shortener
 ```
-3. 編輯.env設定，Windows環境可用記事本編輯
-```
+
+3. Edit the `.env` configuration. For Windows environments, you can use Notepad
+
+```sh
 nano .env
 ```
-4. .env檔填寫如下內容
+
+4. Fill in the `.env` file with the following content:
+
 ```env
 DEBUG=False
 SECRET_KEY=MY_SECRET_KEY
 ```
-5. 使用docker-compose啟動服務
-```
+
+5. Use `docker-compose` to start the service:
+
+```sh
 docker-compose up -d
 ```
-6. 確定docker container有正常啟動，並複製container id
-```
+
+6. Ensure that the docker container has started successfully, and copy the `container id`
+
+```sh
 docker ps
 ```
-7. 首次啟動後，執行資料庫初始化。 <your_container_id>請填入上一步複製的container id
-```
+
+7. After the first startup, perform database initialization. Replace `<your_container_id>` with the container id copied in the previous step:
+
+```sh
 docker exec -ti <your_container_id> python init_db.py
 ```
-8. 服務會啟在 http://127.0.0.1:5000/
 
-9. 停止服務
-```
+8. The server runs at http://127.0.0.1:5000/
+
+9. To stop the server, run:
+
+```sh
 docker-compose down
 ```
+
+## LICENCE
+
+mit
--- app.py
+++ app.py
@@ -3,12 +3,38 @@ from hashids import Hashids
 from flask import Flask, render_template, request, flash, redirect, url_for
 import sqlite3
 import validators
+import json
 import os
+import threading
+from datetime import datetime, timedelta
+from collections import Counter
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "MY_SECRET_KEY")
 redis = Redis(host='redis', port=6379, decode_responses=True)
 hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+
+def redis_listener():
+    while True:
+        try:
+            _, click_data_str = redis.brpop('url_visits', timeout=0)
+            
+            if click_data_str is not None:
+                click_data = json.loads(click_data_str)
+                id = click_data['id']
+                ip_address = click_data['ip_address']
+                user_agent = click_data['user_agent']
+                created = click_data['created']
+                
+                conn = get_db_conn()
+                conn.execute(f"INSERT INTO url_visits (url_id, ip_address, user_agent, created) VALUES ('{id}', '{ip_address}', '{user_agent}', '{created}')")
+                conn.commit()
+                conn.close()
+        except Exception as e:
+            print(f"Error in redis_listener: {e}")
+
+t = threading.Thread(target=redis_listener)
+t.start()
 
 def get_db_conn():
     conn = sqlite3.connect('database.db')
@@ -52,6 +78,15 @@ def url_redirect(id):
         else:
             flash('Invalid URL')
             return redirect(url_for('index'))
+        
+    # send id to redis queue
+    push_data = {
+        "id":hashids.decode(id)[0],
+        "ip_address":request.remote_addr,
+        "user_agent":request.user_agent.string,
+        "created":datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    }
+    redis.rpush('url_visits', json.dumps(push_data))
     return redirect(original_url)
 
 @app.route('/stats')
@@ -62,10 +97,42 @@ def stats():
     urls = []
     for url in db_urls:
         url = dict(url)
-        url['short_url'] = request.host_url + hashids.encode(url['id'])
+        url['created'] = datetime.strptime(url['created'], '%Y-%m-%d %H:%M:%S').strftime('%Y/%m/%d')
+        url['url_id'] = hashids.encode(url['id'])
+        url['short_url'] = request.host_url + url['url_id']
         urls.append(url)
-
     return render_template('stats.html', urls=urls)
+
+def get_nearest_days_data(data, days = 10):
+    dates = [datetime.strptime(entry['created'], '%Y-%m-%d %H:%M:%S').date() for entry in data]
+    latest_date = datetime.now().date()
+    date_labels = [(latest_date - timedelta(days=i)).strftime('%Y/%m/%d') for i in range(days, -1, -1)]
+    date_counts = Counter(dates)
+    counts = [date_counts.get(datetime.strptime(label, '%Y/%m/%d').date(), 0) for label in date_labels]
+    return counts, date_labels
+
+@app.route('/stats/<string:id>')
+def id_stats(id):
+    original_id = hashids.decode(id)[0]
+    conn = get_db_conn()
+    visit_data_exec = conn.execute(f"SELECT id, ip_address, created FROM url_visits WHERE url_id = ({original_id})").fetchall()
+    target_url_result = conn.execute(f"SELECT original_url, created FROM urls WHERE id = ({original_id})").fetchone()
+    url_data = {
+        "target":target_url_result['original_url'],
+        "from":request.host_url + id,
+        "created":datetime.strptime(target_url_result['created'], '%Y-%m-%d %H:%M:%S').strftime('%Y/%m/%d'),
+    }
+    conn.close()
+    
+    visit_data = []
+    for row in visit_data_exec:
+        row = dict(row)
+        visit_data.append(row)
+    url_data["total_visits"] = len(visit_data)
+    values, labels = get_nearest_days_data(visit_data)
+    visits = {"values":values, "labels":labels}
+
+    return render_template('analyze.html', visits=visits, url_data=url_data)
 
 if __name__ == "__main__":
     app.run(debug=os.environ.get("DEBUG", False))
